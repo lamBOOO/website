9f581d693f18a6c809a28285740e83602f468a79
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2021-08-23 13:39:34 +0200

    Add a change

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Index() {
   return (
     <div>
-      <h1> Coming soon </h1>
+      <h1> Coming soon :-)</h1>
       <p>45ca0a3a0b9de29e3dc56ea01aceb70827ac0b16cba015a9e0e5b81be2b84db9</p>
     </div>
   );
