a4492ea1da3223929b186b1e1d065179c12faef8
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2021-08-23 13:19:15 +0200

    Add new secure salted SHA-256 msg
    $ echo -n "Owned by lamBOO @Jv%pHg9HyAuHfnY" | shasum --algorithm 256

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <div>
       <h1> Coming soon </h1>
-      <p>8a0535075f13852aaa4adb62a4090626d1808bbb9ec27c0efd704212846f1d64</p>
+      <p>45ca0a3a0b9de29e3dc56ea01aceb70827ac0b16cba015a9e0e5b81be2b84db9</p>
     </div>
   );
 }
