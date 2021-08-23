f57641539c7086bdaf44610b8977a28de07c20fa
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2021-08-23 13:17:53 +0200

    Add "Owned by lamBOO" as SHA-256 hash
    - Verify on OSX with:
    $ echo -n "Owned by lamBOO " | shasum --algorithm 256

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,8 @@ import Link from "next/link";
 export default function Index() {
   return (
     <div>
-      <h1> Index </h1>
-      <Link href="/about">
-        <a> About </a>
-      </Link>
+      <h1> Coming soon </h1>
+      <p>8a0535075f13852aaa4adb62a4090626d1808bbb9ec27c0efd704212846f1d64</p>
     </div>
   );
 }
